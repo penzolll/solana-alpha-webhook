3e2cf1aeb84ee15f0a9bbe63f29c7d629a9b6bf4
penzolll <matdew444@outlook.com>
2026-08-16 17:53:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,9 +39,10 @@ function connect() {
   console.log('Connecting to Venum WebSocket...');
   ws = new WebSocket(config.VENUM_WSS_URL);
 
-   ws.on('open', () => {
+  ws.on('open', () => {
     console.log('✅ Venum WebSocket connected');
 
+    // Logs Subscribe
     const sub = {
       jsonrpc: '2.0',
       id: 1,
@@ -54,7 +55,6 @@ function connect() {
 
     ws.send(JSON.stringify(sub));
     console.log('📡 Subscribed to Pump.fun program via logsSubscribe');
-  });
 
     if (pingInterval) clearInterval(pingInterval);
     pingInterval = setInterval(() => {
